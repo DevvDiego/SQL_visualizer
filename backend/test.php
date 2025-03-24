164abe2d9cdfdb9656d4af939a151487f27132b3
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-03-24 17:11:14 -0600

    Add ability to fetch the column names and CORS headers for testing

--- backend/test.php
+++ backend/test.php
@@ -1,19 +1,36 @@
 <?php
 include("dbconn.php");
 
-$data = mysqli_query($dbconn, "SELECT * FROM ciudadano");
+//! ##########  TEMPORAL  ##########
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+//! ##########  TEMPORAL  ##########
 
-if (! ($data->num_rows > 0) ) {
+$query_result = mysqli_query($dbconn, "SELECT * FROM ciudadano");
+
+if (! ($query_result->num_rows > 0) ) {
     $dbconn->close();
     exit;
 }
 
-// Convertir todos los resultados directamente a un array asociativo
-$datos = $data->fetch_all(MYSQLI_ASSOC);
+//Get column names
+$columns = [];
+foreach ($query_result->fetch_fields() as $column) {
+    $columns[] = $column->name;
+}
+
+//Get all data in an assoc array
+$datos = $query_result->fetch_all(MYSQLI_ASSOC);
 
 // Imprimir el JSON
 header('Content-Type: application/json');
-echo json_encode($datos);
+echo json_encode(
+    [
+        "columns"=>$columns,
+        "fields"=>$datos
+    ]
+);
 
 $dbconn->close();
 
