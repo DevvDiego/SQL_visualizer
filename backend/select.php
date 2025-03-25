dac84ac034aad1b8dfd9146cb414a72c6ced84e1
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-03-24 18:10:28 -0600

    Move test contents to select.php

---
+++ backend/select.php
@@ -0,0 +1,35 @@
+<?php
+include("dbconn.php");
+
+try{
+    //Get the GET url queryTo parameter
+    $param = "";
+    if( isset( $_GET["queryTo"] ) ){
+        $param = $_GET["queryTo"]; 
+    }
+
+    //Get column names
+    $columns = [];
+    foreach ($query_result->fetch_fields() as $column) {
+        $columns[] = $column->name;
+    }
+
+    //Get all data in an assoc array
+    $datos = $query_result->fetch_all(MYSQLI_ASSOC);
+
+    // Imprimir el JSON
+    header('Content-Type: application/json');
+    echo json_encode(
+        [
+            "columns"=>$columns,
+            "fields"=>$datos
+        ]
+    );
+
+    $dbconn->close();
+    
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
+?>
