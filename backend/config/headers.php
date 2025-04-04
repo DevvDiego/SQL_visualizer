c62f30a6a05b26a195c9763af3316746a89f0b50
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-04 14:34:18 -0600

    add cache headers

--- backend/config/headers.php
+++ backend/config/headers.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 //! ##########  TEMPORAL  ##########
 
+// Evitar almacenamiento en caché
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 header('Content-Type: application/json');
 
