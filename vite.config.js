6c25def4577dcac1b14f3e28da0393c7c117b9fe
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-25 21:37:47 -0600

    Reset project to use sveltekit, keep components but redo structure to work with SVkit routing.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-// import path from "path"
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "frontend"
-  },
-  // resolve: {
-  //   alias: {
-  //     "@": path.resolve(__dirname,"public"),
-  //     "$lib": path.resolve(__dirname,"src/lib"),
-  //   }
-  // }
-})
+	plugins: [tailwindcss(), sveltekit()]
+});
