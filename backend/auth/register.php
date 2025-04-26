7d35c8f07c89638c263db48f86a373b47c22f838
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-04-26 16:05:30 -0600

    add PDO mysql connection

---
+++ backend/auth/register.php
@@ -0,0 +1,9 @@
+<?php
+
+    require("../connect_mysql.php");
+
+    
+    echo(json_encode(["message" => "exito en la conexion creo"]));
+
+
+?>
---
+++ backend/connect_mysql.php
@@ -0,0 +1,23 @@
+<?php
+require("config/headers.php");
+header("Content-Type: application/json");
+
+$credentials = require("config/credentials.php");
+
+$DB_HOST = $credentials["DB_HOST"];
+$DB_NAME = $credentials["DB_NAME"];
+$DB_USER = $credentials["DB_USER"];
+$DB_PASS = $credentials["DB_PASS"];
+
+
+try{
+
+    $pdo = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} 
+catch (PDOException $ex) {
+    die("Error de conexión: " . $ex->getMessage());
+}
+
+?>
